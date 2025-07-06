df4211958afa5bc5857bc86908426243c5ad4901
maianagra <maianagra@hotmail.co.uk>
2025-07-06 20:46:50 +0100

    fix: terraform files fix

--- terraform/acm.tf
+++ terraform/acm.tf
@@ -25,7 +25,7 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
-  provider                = aws.eu_west_1
+  provider                = aws.us_east_1
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
